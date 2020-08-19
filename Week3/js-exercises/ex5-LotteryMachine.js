ec36f9da4c9c3e12954cd841a4ec34cf1c0dbf1b
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-08-19 23:48:50 +0200

    Finished ex5: The lottery machine

--- Week3/js-exercises/ex5-LotteryMachine.js
+++ Week3/js-exercises/ex5-LotteryMachine.js
@@ -2,37 +2,65 @@
  
  ** Exercise 5: The lottery machine **
  
-Don't you just love the thrill of the lottery? What if I told you we can make our own lottery machine? Let'
- s get started!
+Don't you just love the thrill of the lottery? What if I told you we can make our own lottery machine? Let's get started!
 
-   Write a
- function that takes 4 arguments.
+  Write a function that takes 4 arguments.
 
    - A start value
    - An end value
    - A callback that executes if the number is divisible by 3
    - A callback that executes if the number is divisible by 5
 
- The  function should first generate an array containing values from start value to end value(inclusive).
+ //problem 1: The  function should first generate an array containing values from start value to end value(inclusive).
 
- Then the  function should take the newly created array and iterate over it, and calling the first callback
+ //problem2: Then the  function should take the newly created array and iterate over it, and calling the first callback
  if the array value is divisible by 3.
 
- The function should call the second callback
+ //problem3: The function should call the second callback
  if the array value is divisible by 5.
 
- Both functions should be called
+ //problem 4: sBoth functions should be called
  if the array value is divisible by both 3 and 5.
-
 */
 
-function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
-  const numbers = [];
-  // make array
-  // start at beginning of array and check if you should call threeCallback or fiveCallback or go on to next
+//problem 2: divisible by 3
+function threeCallback(number) {
+  return 'is divisible by three';
 }
 
-threeFive(10, 15, sayThree, sayFive);
+//problem3: divisible by 5
+function fiveCallback(number) {
+  return 'is divisible by five';
+}
 
-// Should create an array [10,11,12,13,14,15]
-// and call sayFive, sayThree, sayThree, sayFive
+
+function createListOfNumbersAndCheckIfDivisibleByThreeOrFive(startIndex, stopIndex, threeCallback, fiveCallback) {
+  // make an empty array 
+  const arrayOfNumbers = [];
+  //iterate between the start and stop value, adding 1 to the start value on every iteration
+  while(startIndex <= stopIndex) {
+  arrayOfNumbers.push(startIndex++);
+  }
+
+  // start at beginning of array and check if you should call threeCallback or fiveCallback or go on to next
+  arrayOfNumbers.forEach(i => {
+    //problem 4: call two callbacks on one condition. This condition must be check before the other ones
+    if(i % 3 === 0 && i % 5 === 0){
+      console.log(`${i} ${threeCallback(i)} and ${fiveCallback(i)}`);
+    } 
+    //if index is divisible by 3, call threeCallback
+    else if (i % 3 === 0) {
+      console.log(`${i} ${threeCallback(i)}`);
+    } 
+    //if index is divisible by 5, call fiveCallback
+    else if(i % 5 === 0) {
+      console.log(`${i} ${fiveCallback(i)}`);
+    }
+    
+  });
+
+}
+
+threeFive = createListOfNumbersAndCheckIfDivisibleByThreeOrFive;
+
+threeFive(10, 15, threeCallback, fiveCallback);
